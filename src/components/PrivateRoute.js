58118e6de055bcbb77a5327136c14f6ef886f026
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2021-10-16 16:07:50 -0500

    Arreglo de la validacion de url

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,16 +3,16 @@ import {
     Route,
     Redirect,
 } from "react-router-dom";
-import {useJwt} from 'react-jwt';
+//import {useJwt} from 'react-jwt';
 //validar 
 const PrivateRoute=({children, ...rest})=> {
     let auth=localStorage.getItem("token");
-    const {decodedToken, isExpired}=useJwt(auth);      
+    //const {decodedToken, isExpired}=useJwt(auth);      
     return (
         <Route
             {...rest}
             render={({ location }) =>
-                decodedToken && !isExpired ? (
+                auth ? (
                     children
                 ) : (
                     <Redirect
