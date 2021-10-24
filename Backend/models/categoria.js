44b43c34430ef6bbaa35f1cebc702f4092518416
Cristian23988 <cfbravo@unimayor.edu.co>
2021-10-24 17:14:17 -0500

    Borrado de backend

--- Backend/models/categoria.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const categoriaSchema= mongoose.Schema({
-    nombre_categoria: {type:String, required: true}
-});
-module.exports=mongoose.model("categoria", categoriaSchema);
