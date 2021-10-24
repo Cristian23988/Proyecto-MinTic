282abe31c054a89f026c435a81991af94bf84ca8
Cristian23988 <cfbravo@unimayor.edu.co>
2021-10-24 17:06:07 -0500

    heroku

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var mongoose = require("mongoose");
-const cors=require("cors");
+const cors = require("cors");
 var app = express();
+require("dotenv").config();
 
 const productsRoutes = require("./routes/products");
 const categoriaRoutes = require("./routes/categoria");
@@ -11,9 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 //coneccion a la db mongo
-mongoose.connect("mongodb+srv://alejo:IIrpVJdHq1P7H9KE@cluster0.702jh.mongodb.net/improTicDB?retryWrites=true&w=majority").then(() => {
-    console.log("Estamos conectados")
-});
+mongoose
+    .connect(
+        process.env.MONGODB_CONNECT
+        ).then(() => {
+            console.log("Estamos conectados")
+        });
 
 app.use("/api/Usuarios", UsuarioRoutes);
 app.use("/api/products", productsRoutes);
